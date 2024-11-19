22499b062fc7ba152c74acad690b2a1319ac07ea
Can0001 <tuersaimcan@gmail.com>
2024-11-19 12:43:19 +0300

    arama butonu güncellendi

---
+++ index.js
@@ -0,0 +1,36 @@
+const toggleBtn = document.querySelector('.toggle_btn');
+const toggleBtnIcon = document.querySelector('.toggle_btn i');
+const dropDownMenu = document.querySelector('.dropdown_menu');
+const actionBtn = document.querySelector('.action_btn');
+const searchBox = document.querySelector('.search_box');
+const searchInput = document.querySelector('.search_box input');
+const closeSearchBtn = document.querySelector('.close_search');
+const dropdownActionBtn = document.querySelector('.dropdown_menu .action_btn');  
+
+toggleBtn.onclick = function () {
+    dropDownMenu.classList.toggle('open');
+    const isOpen = dropDownMenu.classList.contains('open');
+    
+    toggleBtnIcon.classList = isOpen
+    ?'fa-solid fa-xmark'
+    :'fa-solid fa-bars'
+}
+
+actionBtn.onclick = function(event) {
+    event.preventDefault();  
+    searchBox.classList.add('open');
+    searchInput.focus();  
+}
+
+closeSearchBtn.onclick = function() {
+    searchBox.classList.remove('open');
+}
+
+// Responsive dropdown'daki action_btn'yi de aynı işlevi görüyor
+if (dropdownActionBtn) {
+    dropdownActionBtn.onclick = function(event) {
+        event.preventDefault();  
+        searchBox.classList.add('open');
+        searchInput.focus();  
+    }
+}
